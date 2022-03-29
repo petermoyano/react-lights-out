a8d9b6a68de24136bd0fdf45f150d1f73fbe4db8
Pedro Moyano <pedromoyano454@gmail.com>
2022-03-29 06:07:58 -0300

    game working correctly: hasWon and tblBoard in Board component

--- src/Board.js
+++ src/Board.js
@@ -28,36 +28,39 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows = 5, ncols = 5, chanceLightStartsOn }) {
-  const [board, setBoard] = useState(createBoard());
+function Board({ nrows = 5, ncols = 5 }) {
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-  function createBoard() {
+  const createBoard = () => {
     let initialBoard = [];
     // TODO: create array-of-arrays of true/false values
     for (let i = 0; i < nrows; i++) {
       initialBoard.push([]);
       for (let j = 0; j < ncols; j++) {
-        initialBoard[i].push(chanceLightStartsOn)
+        initialBoard[i].push(chanceLightStartsOn())
       }
     }
     return initialBoard;
   }
 
+  const [board, setBoard] = useState(createBoard());
+  const initialBoard = createBoard();
+
   function hasWon() {
     // TODO: check the board in state to determine whether the player has won.
     for (let i = 0; i < nrows; i++) {
       for (let j = 0; j < ncols; j++) {
-        if (initialBoard[i][j] === false) {
+        if (initialBoard[i][j] !== true) {
           return false
         }
       }
     }
     return true
   }
+
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
-      const [y, x] = coord.split("-").map(Number);
+      const [y, x] = coord.split("-").map(Number); //takes the string "2-4" and turns it into y = 2, x = 4
 
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
@@ -68,18 +71,47 @@ function Board({ nrows = 5, ncols = 5, chanceLightStartsOn }) {
       };
 
       // TODO: Make a (deep) copy of the oldBoard
+      const boardCopy = oldBoard.map(row => [...row]);
 
       // TODO: in the copy, flip this cell and the cells around it
-
+      flipCell(y, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
+      
       // TODO: return the copy
+      return boardCopy;
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
+  if (hasWon()) {
+    return <div>You Win!</div>;
+  }
 
-  // TODO
+  // make table board: rows of Cell components
+  let tblBoard = [];
+  for (let y = 0; y < nrows; y++) {
+    let row = [];
+    for (let x = 0; x < ncols; x++) {
+      let coord = `${y}-${x}`;
+      row.push(
+        <Cell
+          key={coord}
+          isLit={board[y][x]}
+          flipCellsAroundMe={() => flipCellsAround(coord)}
+        />
+      );
+    }
+    tblBoard.push(<tr key={y}>{row}</tr>);
+  }
 
-  // make table board
+  return (
+    <table className="Board">
+      <tbody>{tblBoard}</tbody>
+    </table>
+  );
 
   // TODO
 }
